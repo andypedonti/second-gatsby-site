2f3628182e70f79180617377ca10c17eeedb3f96
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2021-12-22 09:20:55 -0500

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "second-gatsby-site",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
